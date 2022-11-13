60bc313fd391be5fc91be872589cb9c74e4d845a
linj1016 <linwj1016@gmail.com>
2022-11-13 10:05:54 -0500

    Added a new data method
    added data method obtainExtraData to get more samples from the available datasets. Also added a new dataset derived from the spam/ham dataset that is labeled by Irene

--- data_methods.py
+++ data_methods.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 import numpy as np
 from collections import defaultdict
-
+import random
 
 def filter_for_data(filename):
     return "sample_data" in filename and filename.endswith(".csv")
@@ -37,4 +37,51 @@ def get_initdata(): #return title, text and label
         else:
             Type.append(0)
     dataDict["Type"] = Type
+    return dataDict
+
+def obtainExtraData(nCount, cCount, pCount):       #obtain x amount of datasets from the available dataset, pcount = phishing count, ccount = commercial spam count, ncount = normal email count
+    pdbyType = pd.read_csv("./data/PhishingEmailData.csv", encoding = "ISO-8859-1")
+    SpamHamDS = pd.read_csv("./data/spam_ham_dataset.csv", encoding = "ISO-8859-1")
+    CEmailDS = pd.read_csv("./data/commercial email.csv", encoding = "ISO-8859-1")
+    SubjectList, TextList, TypeList = [], [], []
+    PhishingSubject, PhishingMessage = list(pdbyType["Email_Subject"]), list(pdbyType["Email_Content"])
+    SHText, SHLabel= list(SpamHamDS["text"]), list(SpamHamDS["label"])
+    CommercialSubject, CommercialText = [], []
+    SHSubject, SHMessage = [], []
+    
+    dataDict = defaultdict() 
+
+    for i in range(len(SHText)):            
+        if SHLabel[i] == "ham":
+            temp = SHText[i].split('\n')
+            SHSubject.append(temp[0][8:])
+            SHMessage.append(SHText[i][len(temp[0]):])
+
+    pos = random.sample(range(len(SHSubject)), nCount)
+    for i in pos:
+        SubjectList.append(SHSubject[i])
+        TextList.append(SHMessage[i])
+        TypeList.append(0)
+    for i in range(len(CEmailDS["message"])):
+        if CEmailDS["type"][i] == "0":
+            temp = str(CEmailDS["message"][i]).split("\n")
+            PhishingSubject.append(temp[0][8:])
+            PhishingMessage.append(str(CEmailDS["message"][i])[len(temp[0]):])
+        elif CEmailDS["type"][i] == "1":
+            temp = str(CEmailDS["message"][i]).split("\n")
+            CommercialSubject.append(temp[0][8:])
+            CommercialText.append(str(CEmailDS["message"][i])[len(temp[0]):])
+    pos = random.sample(range(len(PhishingSubject)), pCount)
+    for i in pos:
+        SubjectList.append(PhishingSubject[i])
+        TextList.append(PhishingMessage[i])
+        TypeList.append(2)
+    pos = random.sample(range(len(CommercialSubject)), cCount)
+    for i in pos:
+        SubjectList.append(CommercialSubject[i])
+        TextList.append(CommercialText[i])
+        TypeList.append(1)
+    dataDict["Subject"] = SubjectList
+    dataDict["Text"] = TextList
+    dataDict["Type"] = TypeList
     return dataDict
