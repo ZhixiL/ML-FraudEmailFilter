bd6bc6466e5c541e6fd320184f72292e8c9990d8
linj1016 <linwj1016@gmail.com>
2022-11-22 21:55:00 -0500

    updated model.py to add Naive Bayes

--- Model.py
+++ Model.py
@@ -2,6 +2,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression as LR
+from sklearn.naive_bayes import GaussianNB, MultinomialNB, ComplementNB, BernoulliNB
 from sklearn.ensemble import RandomForestClassifier
 from data_methods import getAllData, overSampling
 from nlp import nlp
@@ -36,3 +37,23 @@ if __name__ == '__main__':
     RFModel = RandomForestClassifier(max_depth=2, random_state=0)
     RFModel.fit(cont_train, lab_train)
     print(f"Random Forest of the model is {RFModel.score(cont_test, lab_test)}")
+
+    #Gaussian Naive Bayes
+    gaussNB = GaussianNB()
+    gaussNB.fit(cont_train.toarray(), lab_train)
+    print(f"Gaussian Naive Bayes score of the model is {gaussNB.score(cont_test.toarray(), lab_test)}")
+
+    #Multinomial Naive Bayes
+    multiNB = MultinomialNB()
+    multiNB.fit(cont_train.toarray(), lab_train)
+    print(f"Multinomial Naive Bayes score of the model is {multiNB.score(cont_test.toarray(), lab_test)}")                
+
+    #Complement Naive Bayes
+    compNB = ComplementNB()
+    compNB.fit(cont_train.toarray(), lab_train)
+    print(f"Complement Naive Bayes score of the model is {compNB.score(cont_test.toarray(), lab_test)}")               
+
+    #Bernoulli Naive Bayes
+    berNB = BernoulliNB()
+    berNB.fit(cont_train.toarray(), lab_train)
+    print(f"Bernoulli Naive Bayes score of the model is {berNB.score(cont_test.toarray(), lab_test)}") 
